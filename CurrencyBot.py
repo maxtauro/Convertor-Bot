cfb413900d91066fcdf500a4a91d7e2e029f3ba1
maxtauro <maxtauro@hotmail.com>
2017-08-22 20:07:32 -0400

    added lb and kg conversion helper functions

--- CurrencyBot.py
+++ CurrencyBot.py
@@ -4,6 +4,24 @@ import praw
 import check
 import time
 
+
+#convert from lbs to kg
+def converttoKg(toConvert,converted): #converted is a listof(Num, Num) representing (lbs,kgs)
+    if toConvert == []:
+        return converted
+    else:
+        converted.append([toConvert[0],(toConvert[0]/2.2)])
+        return converttoKg(toConvert[1:],converted)
+    
+#convert from kg to lbs
+def converttoLbs(toConvert,converted): #converted is a listof(Num, Num) representing (lbs,kgs)
+    if toConvert == []:
+        return converted
+    else:
+        converted.append([toConvert[0],(toConvert[0]*2.2)])
+        return converttoLbs(toConvert[1:],converted)    
+    
+
 def bot_login():
     currencyBot =  praw.Reddit(user_agent='CurrencyBot v0.1',
                                client_id='POmsiu3RKBxEKg',
@@ -28,7 +46,8 @@ def run_bot(currencyBot):
             for i in range (1,len(text)):
                 if (text[i] == 'kg' or text[i] == 'kgs') and text[i-1].isdigit():
                     toConvert.append(text[i-1])
-                    print(toConvert) 
+                    #print(toConvert) 
+            
             #Generate message
             message = "Congrats Max ur siccc"
             ##comment.reply(message) #send message
@@ -39,12 +58,12 @@ def run_bot(currencyBot):
             for i in range (1,len(text)):
                 if (text[i] == 'lb' or text[i] == 'lbs') and text[i-1].isdigit():
                     toConvert.append(text[i-1])
-                    print(toConvert)             
+                    #print(toConvert)             
             
             #Generate message
             message = "Congrats Max ur siccc"
             ##comment.reply(message) #send message
-            print("lb was brough up in {0} in {1}".format(text,comment.submission.title))  
+            print("lb was brought up in {0} in {1}".format(text,comment.submission.title))  
             print('/n')
             
             #Call conversion from lb to kg
@@ -58,10 +77,24 @@ def run_bot(currencyBot):
     time.sleep(10)
     
 
+##Tests
+    
+#ConverttoKG
+check.expect("Convert to kg one item",converttoKg([2.2],[]),[[2.2,1]])
+check.within("Convert to kg multiple items",converttoKg([2.2,100,135,1],[]),[[2.2, 1.0], [100, 45.4545], [135, 61.363], [1, 0.4545]],0.001)
 
-r = bot_login()
-while True:
-    run_bot(r)
+#ConverttoLbs
+check.expect("Convert to lbs one item",converttoLbs([1],[]),[[1 ,2.2]])
+check.within("Convert to lbs multiple items",converttoLbs([2.2,100,135,1],[]),[[2.2, 4.84], [100, 220.0], [135, 297.0], [1, 2.2]],0.001)
+
+
+
+#r = bot_login()
+#while True:
+#    run_bot(r)
+
+
+
 
 
 
